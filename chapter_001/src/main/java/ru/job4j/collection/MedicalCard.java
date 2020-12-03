be4880967eb041a791dd766b2ef3bffd12fbea29
Shestakov Evgeny <lester_g@inbox.ru>
2020-12-03 13:45:51 +0300

    2. Без переопределения equals и hashCode [#399519]

---
+++ chapter_001/src/main/java/ru/job4j/collection/MedicalCard.java
@@ -0,0 +1,9 @@
+package ru.job4j.collection;
+
+public class MedicalCard {
+    int id;
+
+    public MedicalCard(int id) {
+        this.id = id;
+    }
+}
--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -1,9 +1,35 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     String name;
     int children;
     Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>();
+        User user1 = new User("Наташа", 2, new GregorianCalendar(1995, Calendar.
+                APRIL, 23));
+        User user2 = new User("Наташа", 2, new GregorianCalendar(1995, Calendar.
+                APRIL, 23));
+        map.put(user1, new MedicalCard(5));
+        map.put(user2, new MedicalCard(5));
+        for (Map.Entry<User, Object> pair : map.entrySet()) {
+            User key = pair.getKey();
+            Object value = pair.getValue();
+            System.out.println(key + ":" + value);
+        }
+
+
+    }
 }
