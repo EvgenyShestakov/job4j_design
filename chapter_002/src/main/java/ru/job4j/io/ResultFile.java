dff3a0b6eefa14bf4e940a6b2a66d96a84bd95c4
Shestakov Evgeny <lester_g@inbox.ru>
2021-01-23 14:36:27 +0300

    0.1. FileOutputStream. [#399535]

---
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i < 10; i++) {
+                for (int k = 1; k < 10; k++) {
+                    out.write((k * i + "  ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
