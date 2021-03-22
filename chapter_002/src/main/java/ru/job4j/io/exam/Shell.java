bb06f32eedaa5d662eccb42fd82d4860b8e8a46e
Shestakov Evgeny <lester_g@inbox.ru>
2021-03-22 13:28:47 +0300

    Симуляция терминала Linux

--- chapter_002/src/main/java/ru/job4j/io/exam/Shell.java
+++ chapter_002/src/main/java/ru/job4j/io/exam/Shell.java
@@ -1,19 +1,43 @@
 package ru.job4j.io.exam;
 
+import java.util.Stack;
+
 public class Shell {
-    private String path = "/";
+    private Stack<String> path = new Stack<>();
+    { path.push("/"); }
 
     public void cd(String string) {
-        if (string.equals("/") || string.equals("..") || string.endsWith("..")) {
-            path = "/";
-        } else if (path.equals("/")) {
-            path = String.format("%s%s", "/", string);
+        if (string.equals("/")) {
+            while (path.size() > 1) {
+                path.pop();
+            }
         } else {
-            path = String.format("%s%s%s", path, "/", string);
+            String[] array = string.split("/");
+            for (String step : array) {
+                path.push(step);
+            }
+            boolean flag = true;
+            while (path.peek().equals("..")) {
+                path.pop();
+                if (!path.peek().equals("..") && !path.peek().equals("/") && flag) {
+                    path.pop();
+                    flag = false;
+                }
+            }
         }
     }
 
     public String pwd() {
-        return path;
+        StringBuilder stringBuilder = new StringBuilder();
+        String folder = "";
+        for (String step : path) {
+            if (step.equals("/") || folder.equals("/")) {
+                folder = step;
+            } else {
+                folder = String.format("%s%s", "/", step);
+            }
+            stringBuilder.append(folder);
+        }
+        return stringBuilder.toString();
     }
 }
