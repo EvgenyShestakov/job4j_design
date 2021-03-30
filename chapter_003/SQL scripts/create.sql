c474cd2f16ba6a21df9b4f65e16afc9f613f0e24
Shestakov Evgeny <lester_g@inbox.ru>
2021-03-30 11:51:57 +0300

    1. Схема прав пользователи и роли. [#399464]

--- chapter_003/SQL scripts/create.sql
+++ chapter_003/SQL scripts/create.sql
@@ -10,12 +10,16 @@ create table role(
     id serial primary key,
     name varchar(200)
 );
-create table users(
-    id serial primary key,
-    name varchar(200),
-    role_id int references role(id)
-);
 create table rules(
+    id serial primary key,
+    name varchar(200)
+);
+create table role_rules(
+    id serial primary key,
+    role_id int references role(id),
+    rules_id int references rules(id)
+);
+create table users(
     id serial primary key,
     name varchar(200),
     role_id int references role(id)
@@ -37,5 +41,3 @@ create table attachs(
     name varchar(200),
     item_id int references  item(id)
 );
-ALTER TABLE role
-add rules_id int references rules(id);
--- chapter_003/SQL scripts/insert.sql
+++ chapter_003/SQL scripts/insert.sql
@@ -7,12 +7,15 @@ insert into state(name) values ('State3');
 insert into role(name) values ('Role1');
 insert into role(name) values ('Role2');
 insert into role(name) values ('Role3');
+insert into rules(name) values ('Rules1');
+insert into rules(name) values ('Rules2');
+insert into rules(name) values ('Rules3');
+insert into role_rules(role_id, rules_id) values (1, 3);
+insert into role_rules(role_id, rules_id) values (2, 1);
+insert into role_rules(role_id, rules_id) values (3, 2);
 insert into users(name, role_id) values ('User1', 1);
 insert into users(name, role_id) values ('User2', 2);
 insert into users(name, role_id) values ('User3', 3);
-insert into rules(name, role_id) values ('RuleOne', 1);
-insert into rules(name, role_id) values ('RuleTwo', 2);
-insert into rules(name, role_id) values ('RuleThree', 3);
 insert into item(name, users_id, category_id, state_id) values ('Item1', 1, 2, 3);
 insert into item(name, users_id, category_id, state_id) values ('Item2', 2, 1, 2);
 insert into item(name, users_id, category_id, state_id) values ('Item3', 3, 3, 1);
@@ -22,9 +25,3 @@ insert into comments(name, item_id) values ('Comments3', 1);
 insert into attachs(name, item_id) values ('Attachs1', 3);
 insert into attachs(name, item_id) values ('Attachs2', 2);
 insert into attachs(name, item_id) values ('Attachs3', 1);
-update role set rules_id = 1
-   where name = 'Role1';
-update role set rules_id = 2
-   where name = 'Role2';
-update role set rules_id = 3
-   where name = 'Role3';
