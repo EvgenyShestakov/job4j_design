a594f4663d8f2b646fe614b86d1952a6c1a04d7a
Shestakov Evgeny <lester_g@inbox.ru>
2021-04-11 14:59:51 +0300

    0.1. Statement [#399440]

--- chapter_003/src/main/java/ru/job4j/jdbc/TableEditor.java
+++ chapter_003/src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -3,13 +3,14 @@ package ru.job4j.jdbc;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import java.util.Objects;
 import java.util.Properties;
 
 public class TableEditor implements AutoCloseable {
     private Connection connection;
     private final Properties properties;
 
-    public TableEditor(Properties properties) throws SQLException {
+    public TableEditor(Properties properties) {
         this.properties = properties;
         initConnection();
     }
@@ -26,67 +27,76 @@ public class TableEditor implements AutoCloseable {
         } catch (IOException | ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
-
     }
 
-    public void createTable(String tableName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "create table if not exists %s();", tableName);
-            statement.execute(sql);
-        }
+    public void createTable(String tableName) {
+        String sql = String.format(
+                "create table if not exists %s();", tableName);
+        executionRequest(sql);
     }
 
-    public void dropTable(String tableName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "drop table if exists %s;", tableName);
-            statement.execute(sql);
-        }
+    public void dropTable(String tableName) {
+        String sql = String.format(
+                "drop table if exists %s;", tableName);
+        executionRequest(sql);
     }
 
-    public void addColumn(String tableName, String columnName, String type) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "alter table %s add %s %s;", tableName, columnName, type);
-            statement.execute(sql);
-        }
+    public void addColumn(String tableName, String columnName, String type) {
+        String sql = String.format(
+                "alter table %s add %s %s;", tableName, columnName, type);
+        executionRequest(sql);
     }
 
-    public void dropColumn(String tableName, String columnName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "alter table %s drop %s;", tableName, columnName);
-            statement.execute(sql);
-        }
+    public void dropColumn(String tableName, String columnName) {
+        String sql = String.format(
+                "alter table %s drop %s;", tableName, columnName);
+        executionRequest(sql);
     }
 
-    public void renameColumn(String tableName, String columnName, String newColumnName) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "alter table %s rename %s to %s;", tableName, columnName, newColumnName);
-            statement.execute(sql);
-        }
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        String sql = String.format(
+                "alter table %s rename %s to %s;", tableName, columnName, newColumnName);
+        executionRequest(sql);
     }
 
-    public String getScheme(String tableName) throws SQLException {
+    public String getScheme(String tableName) {
         StringBuilder scheme = new StringBuilder();
-        DatabaseMetaData metaData = connection.getMetaData();
-        try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
+        DatabaseMetaData metaData = null;
+        try {
+            metaData = connection.getMetaData();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try (ResultSet columns = Objects.requireNonNull(metaData).
+                getColumns(null, null, tableName, null)) {
             scheme.append(String.format("%-15s %-15s%n", "column", "type"));
             while (columns.next()) {
                 scheme.append(String.format("%-15s %-15s%n",
                         columns.getString("COLUMN_NAME"),
                         columns.getString("TYPE_NAME")));
             }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
         return scheme.toString();
     }
 
+    private void executionRequest(String sql)  {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
     @Override
-    public void close() throws Exception {
+    public void close()  {
         if (connection != null) {
-            connection.close();
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
     }
 }
