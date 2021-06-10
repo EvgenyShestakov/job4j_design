c12c2695445232becae22aa057f97a6da30156bd
EvgenyShestakov <lester_g@inbox.ru>
2021-06-10 09:36:49 +0300

    Реализации кеша на SoftReference [#1592 #165281]

--- chapter_004/src/main/java/ru/job4j/cache/AbstractCache.java
+++ chapter_004/src/main/java/ru/job4j/cache/AbstractCache.java
@@ -1,25 +1,26 @@
 package ru.job4j.cache;
 
 import java.io.IOException;
-import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractCache<K, V> {
     protected final Map<K, SoftReference<V>> cache = new HashMap<>();
-    private final ReferenceQueue<V> queue = new ReferenceQueue<>();
 
     public void put(K key, V value) {
-        cache.put(key, new SoftReference<V>(value, queue));
+        cache.put(key, new SoftReference<V>(value));
     }
 
     public V get(K key) throws IOException {
-        if (cache.get(key) == null || cache.get(key).isEnqueued()) {
-            put(key, load(key));
+        V value = cache.getOrDefault(key, new SoftReference<V>(null)).get();
+        if (value == null) {
+            value = load(key);
+            put(key, value);
         }
-       return cache.get(key).get();
+        return value;
     }
-
     protected abstract V load(K key) throws IOException;
 }
+
+
